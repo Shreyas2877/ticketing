71d018446cd283f2019bf31649d89cf60d8eb0ae
Trojan2877 <shreyas.raviprakash@gmail.com>
2024-10-16 17:05:01 +0100

    pushing latest stable build

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,12 +1,17 @@
+import { CustomError } from "./custom-errors";
 
 
-export class DatabaseConnectionError extends Error{
-
+export class DatabaseConnectionError extends CustomError{
+    statusCode = 500;
     reason = "error Connecting to the Database!";
 
     constructor(){
-        super();
+        super("error Connecting to the Database!");
 
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
     } 
+
+    serializeErrors() {
+        return [{ message: this.reason }];
+    }
 }
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,22 +1,12 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-errors";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     
-    if(err instanceof RequestValidationError){
-        const formattedErrors = err.errors.map((error) => {
-            if(error.type === 'field'){
-                return { message: error.msg, field: error.path };
-            }
-        });
-        return res.status(400).send({errors: formattedErrors});
-    }
+    if(err instanceof CustomError){
+        return res.status(err.statusCode).send({errors: err.serializeErrors()});
+    } 
 
-    if(err instanceof DatabaseConnectionError){
-        return res.status(500).send({ errors : [{ message: err.reason }]});
-    }
-    
     res.status(400).send({
         errors: [{ message: "Something went wrong" }]
     });
